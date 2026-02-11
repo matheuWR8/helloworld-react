128965f6d6b9adea0d6c0d4bccb8b43c10516226
matheuwr8 <matheusswr8@gmail.com>
2026-02-10 22:38:50 -0300

    Commit inicial

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Touchable, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [nome, setNome] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>HELLO WORLDoooo!!</Text>
+
+      <Text style={styles.lblNome}>
+        Digite seu nome
+      </Text>
+
+      <TextInput
+        keyboardType='default'
+        style={styles.caixaTexto}
+        onChangeText={setNome}
+        //onChangeText={(texto) => setNome(texto)}
+      >
+
+      </TextInput>
+
+      <TouchableOpacity style={styles.botao}
+        onPress={() => exibeTexto()}  
+      >
+        <Text>CLICA</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 25,
+    color: '#ff0000',
+    fontWeight: 'bold'
+  },
+  lblNome: {
+    marginTop: 40,
+    fontSize: 25,
+    color: "#aaf",
+    borderBottomColor: "#faa",
+    borderBottomWidth: 4,
+  },
+  caixaTexto: {
+    fontSize: 20,
+    width: "90%",
+    borderWidth: 2,
+    borderColor: "#000",
+    marginTop: 20,
+    borderRadius: 15,
+    textAlign: 'center',
+  },
+  botao: {
+    width: "80%",
+    height: 50,
+    backgroundColor: "#00f",
+    borderRadius: 20,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
